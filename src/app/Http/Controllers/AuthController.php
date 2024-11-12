c1a57140b7a8302241d2e3109fc0b24bac355740
eto0831 <eto0831@gmail.com>
2024-11-13 01:32:53 +0900

    feat: Add jsQR as a submodule

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -15,9 +15,11 @@ class AuthController extends Controller
     public function index()
     {
         $reservations = auth()->user()->reservations()->with('shop')->get();
+        $reservation = auth()->user()->reservations()->first();
         $areas = Area::all();
         $genres = Genre::all();
         $favorites = auth()->user()->favorites()->with(['shop.area', 'shop.genre'])->get();
-        return view('mypage.index', compact('reservations', 'areas', 'genres','favorites'));
+        $qrcode = QrCode::size(200)->generate(route('reservation.verify', $reservation->id));
+        return view('mypage.index', compact('reservations', 'reservation', 'areas', 'genres','favorites'));
     }
 }
